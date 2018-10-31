0d8e70373e79d888c44107be78f1e288d8415b71
wangmingxin1 <wangmingxin1>
2018-10-31 14:37:42 +0800

    203.Remove Linked List Elements

---
+++ src/main/java/pers/wmx/leetcode/LeetCode_203_RemoveLinkedListElements.java
@@ -0,0 +1,31 @@
+package pers.wmx.leetcode;
+
+/**
+ * @author: wangmingxin1
+ * @create: 2018-10-31 14:15
+ **/
+public class LeetCode_203_RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        //删除节点时，如果是头结点需要特殊处理
+        //创建一个虚拟头结点，可以简化这一操作
+
+        ListNode newHead = new ListNode(0);
+        newHead.next=head;
+        //遍历链表的指针
+        ListNode cur = newHead;
+        ListNode next = null;
+
+        while(cur.next!=null){
+            next=cur.next;
+            if(next.val==val){
+                cur.next=next.next;
+            }else{
+                cur = cur.next;
+            }
+        }
+
+        return newHead.next;
+
+    }
+}
+
