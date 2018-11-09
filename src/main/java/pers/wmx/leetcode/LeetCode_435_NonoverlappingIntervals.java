9b963f758e561c65e14c339d0bbd40ed2b0b169f
wangmingxin1 <wangmingxin1>
2018-11-09 18:14:00 +0800

    435. Non-overlapping Intervals

---
+++ src/main/java/pers/wmx/leetcode/LeetCode_435_NonoverlappingIntervals.java
@@ -0,0 +1,20 @@
+package pers.wmx.leetcode;
+
+/**
+ * @author: wangmingxin1
+ * @create: 2018-11-09 16:56
+ **/
+public class LeetCode_435_NonoverlappingIntervals {
+    public int eraseOverlapIntervals(Interval[] intervals) {
+
+
+        return 0;
+    }
+}
+
+class Interval {
+  int start;
+  int end;
+  Interval() { start = 0; end = 0; }
+  Interval(int s, int e) { start = s; end = e; }
+}
