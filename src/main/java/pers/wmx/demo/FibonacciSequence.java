4c170a9757c14afe88c3d74385fa48e15a8131b0
wangmingxin1 <wangmingxin1>
2018-11-15 11:06:32 +0800

    斐波那契数列

--- src/main/java/pers/wmx/demo/FibonacciSequence.java
+++ src/main/java/pers/wmx/demo/FibonacciSequence.java
@@ -1,5 +1,9 @@
 package pers.wmx.demo;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 /**
  * 斐波那契数列  f(n) = f(n-1)+f(n-2)
  *
@@ -21,7 +25,7 @@ public class FibonacciSequence {
         return fib1(n-1) + fib1(n-2);
     }
 
-    //非递归
+    //非递归   消除重复计算
     public static int fib2(int n)
     {
         int []fib=new int[50];
@@ -34,4 +38,42 @@ public class FibonacciSequence {
         return fib[n];
     }
 
+
+    //记忆搜索法
+    public static int fib3(int n)
+    {
+        int[] record = new int[n+1];
+        Arrays.fill(record,-1);
+        return fib(n,record);
+    }
+
+    private static int fib(int n, int[] record) {
+       if(n==0){
+           return 0;
+       }
+       if(n==1){
+           return 1;
+       }
+
+       if(record[n]!=-1){
+           return record[n];
+       }
+
+        for (int i = 2; i < n+1 ; i++) {
+            record[i] = fib(i - 1, record) + fib(i - 2, record);
+        }
+
+        return record[n];
+
+    }
+
+    @Test
+    public void test(){
+        System.out.println(fib3(0));
+        System.out.println(fib3(1));
+        System.out.println(fib3(2));
+        System.out.println(fib3(3));
+        System.out.println(fib3(4));
+        System.out.println(fib3(5));
+    }
 }
