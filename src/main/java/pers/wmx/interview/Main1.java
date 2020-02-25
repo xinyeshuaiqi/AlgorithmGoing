f61a313a7262b09e644c13c93c38d04219ad51a9
wang.wmx <wang.wmx@alibaba-inc.com>
2020-02-25 20:15:20 +0800

    update

---
+++ src/main/java/pers/wmx/interview/Main1.java
@@ -0,0 +1,52 @@
+package pers.wmx.interview;
+
+/**
+ * @author wmx
+ * @date 2020-02-25
+ *
+ * 算法实现：
+ *  * 在大文本中查找最长的回文串（至少2个字符，该字符串反序之后同原字符串一样），
+ *  * 并返回（如果没有返回Null）【测试用例：aaabbaccbababac，返回babab】。
+ */
+public class Main1 {
+
+    public String findStr(String s) {
+
+        int len = s.length();
+        if (len < 2) {
+            return null;
+        }
+
+        int maxLen = 1;
+        String res = s.substring(0, 1);
+        // 中心位置枚举到 len - 2 即可
+        for (int i = 0; i < len - 1; i++) {
+            String oddStr = centerSpread(s, i, i);
+            String evenStr = centerSpread(s, i, i + 1);
+            String maxLenStr = oddStr.length() > evenStr.length() ? oddStr : evenStr;
+            if (maxLenStr.length() > maxLen) {
+                maxLen = maxLenStr.length();
+                res = maxLenStr;
+            }
+        }
+        return res;
+    }
+
+    private String centerSpread(String s, int left, int right) {
+        // left = right 的时候，此时回文中心是一个字符，回文串的长度是奇数
+        // right = left + 1 的时候，此时回文中心是一个空隙，回文串的长度是偶数
+        int len = s.length();
+        int i = left;
+        int j = right;
+        while (i >= 0 && j < len) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i--;
+                j++;
+            } else {
+                break;
+            }
+        }
+        // 这里要小心，跳出 while 循环时，恰好满足 s.charAt(i) != s.charAt(j)，因此不能取 i，不能取 j
+        return s.substring(i + 1, j);
+    }
+}
